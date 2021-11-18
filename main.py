348fac73005c18802ce991f8ddb993b5e7f91e6b
Валерия <leriia-spb@yandex.ru>
2021-11-18 20:26:08 +0300

    new rounds

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 import sys
 
 import random
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QPainter, QPaintEvent, QPen
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QColorDialog
-from UI import Ui_MainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("Yellow rounds")
+        MainWindow.resize(427, 388)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.round = QtWidgets.QPushButton(self.centralwidget)
+        self.round.setGeometry(QtCore.QRect(350, 320, 75, 23))
+        self.round.setObjectName("round")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 427, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("Yellow rounds", "Yellow rounds"))
+        self.round.setText(_translate("Yellow rounds", "Круги)"))
 
 
 class Window(QMainWindow, Ui_MainWindow):
@@ -28,7 +55,7 @@ class Window(QMainWindow, Ui_MainWindow):
 
     def draw(self, qp: QPainter):
         r = random.randint(5, 100)
-        pen = QPen(Qt.yellow, 3, Qt.SolidLine)
+        pen = QPen(random.choice([Qt.yellow, Qt.black, Qt.red, Qt.blue, Qt.gray, Qt.green, Qt.white]), 3, Qt.SolidLine)
         qp.setPen(pen)
         qp.drawEllipse(random.randint(50, 350), random.randint(50, 350), r, r)
 
